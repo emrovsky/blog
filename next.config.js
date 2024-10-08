bac9eb67a7b21022dafac1fbdd15874bf8bb8a5b
Emre <85563550+emrovsky@users.noreply.github.com>
2024-10-09 00:59:14 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,18 +4,19 @@
  * @type {import('next').NextConfig}
  **/
 const config = {
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 
-    async redirects() {
-        return [
-            {
-                source: '/post/:slug',
-                permanent: true,
-            },
-        ];
-    },
+	async redirects() {
+		return [
+			{
+				source: '/post/:slug',
+				destination: '/post/:slug',
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = config;
